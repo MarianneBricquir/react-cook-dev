af58f7a99cd32c357c6702243bcc02da28fcc633
MarianneBricquir <marianne.bricquir@gmail.com>
2022-10-04 13:03:59 +0200

    meals card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 const Card = ({ meal }) => {
@@ -10,10 +9,9 @@ const Card = ({ meal }) => {
         <div>
             <div className="meal-card" onClick={() => navigate(`/${meal.idMeal}`)}>
                 <h2 className="h-meal">{meal.strMeal}</h2>
-                <img className="img-meal" src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
                 <p className="p-origin">Origin : {meal.strArea}</p>
                 <div className="meals-category">{meal.strCategory}</div>
-
+                <img className="img-meal" src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
             </div>
         </div>
     );
